7efef35b4d968aae5ee15a469d56f4e0e55048f4
Ruslan-Vamos <ruslan_yurich@mail.ru>
2024-12-18 23:38:09 +0300

    1.2.1.6. Правила Checkstyle [#504981 #403385]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,21 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+    public static final String NEWVALUE = "";
+    private int sizeOfEmpty = 10;
+    private String surname;
+    private String name;
+
+    public Broken() { }
+
+    public void echo() { }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) { }
+}
