b3254d40886e8ac32ede8361cf8f361e661a77ef
Ruslan-Vamos <ruslan_yurich@mail.ru>
2025-01-16 01:44:41 +0300

    1.2.4.5. Tracker - хранилище [#396 #31850]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,28 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] rst = new Item[items.length];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i] != null) {
+                rst[count++] = items[i];
+            }
+        }
+        return Arrays.copyOf(rst, count);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rst = new Item[items.length];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName() != null && key.equals(items[i].getName())) {
+                rst[count++] = items[i];
+            }
+        }
+        return Arrays.copyOf(rst, count);
     }
 
     public Item findById(int id) {
