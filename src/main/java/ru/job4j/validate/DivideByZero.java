bab7f8c1ba1bae7c39e2e821453b5247e2a12ab1
Ruslan-Vamos <ruslan_yurich@mail.ru>
2025-01-17 00:47:09 +0300

    1.2.4.8. Что такое валидация? [#246864 #31842]

---
+++ src/main/java/ru/job4j/validate/DivideByZero.java
@@ -0,0 +1,19 @@
+package ru.job4j.validate;
+
+public class DivideByZero {
+
+    public static int divide(int first, int second) {
+        int result = -1;
+        if (second == 0) {
+            System.out.println("Div by 0, return value -1");
+        } else {
+            result = first / second;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int result = divide(10, 0);
+        System.out.println(result);
+    }
+}
