335861d9120ba81af4f038956ee57d6f238c6c86
Ruslan-Vamos <ruslan_yurich@mail.ru>
2025-01-18 05:38:12 +0300

    1.2.4.10. toString [#310018 #407695]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,12 +1,9 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
 
     public static void main(String[] args) {
         Item test = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yy HH:mm:ss");
-        System.out.println(test.getCreated().format(formatter));
+        System.out.println(test);
     }
 }
