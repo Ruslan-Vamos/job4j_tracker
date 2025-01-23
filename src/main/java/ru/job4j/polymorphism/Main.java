36284f331fea6c4be339fb31ecfa4122e5185e9a
Ruslan-Vamos <ruslan_yurich@mail.ru>
2025-01-23 22:32:11 +0300

    1.2.5.4.0. Что такое Interface. [#504946 #409755]

---
+++ src/main/java/ru/job4j/polymorphism/Main.java
@@ -0,0 +1,13 @@
+package ru.job4j.polymorphism;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Vehicle vehicle = new SportCar();
+        vehicle.changeGear();
+        vehicle.accelerate();
+        vehicle.steer();
+        vehicle.brake();
+        vehicle.refill();
+    }
+}
