3d50e2d0a0b699f245370439ff2abdaaea9e2c92
Ruslan-Vamos <ruslan_yurich@mail.ru>
2025-01-29 23:56:23 +0300

    1.2.5.12.1 Зависимость от System.out [#33568 #31806]

--- src/main/java/ru/job4j/tracker/ReplaceAction.java
+++ src/main/java/ru/job4j/tracker/ReplaceAction.java
@@ -1,6 +1,12 @@
 package ru.job4j.tracker;
 
 public class ReplaceAction implements UserAction {
+    private final Output output;
+
+    public ReplaceAction(Output output) {
+        this.output = output;
+    }
+
     @Override
     public String name() {
         return "Изменить заявку";
@@ -8,14 +14,14 @@ public class ReplaceAction implements UserAction {
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        System.out.println("=== Редактирование заявки ===");
+        output.println("=== Редактирование заявки ===");
         int id = input.askInt("Введите id заявки, которую хотите заменить:");
         String name = input.askStr("Введите название новой заявки:");
         Item item = new Item(name);
         if (tracker.replace(id, item)) {
-            System.out.println("Заявка изменена. Новая заявка с id " + id + ": " + item);
+            output.println("Заявка изменена. Новая заявка с id " + id + ": " + item);
         } else {
-            System.out.println("Такого id в хранилище не существует.");
+            output.println("Такого id в хранилище не существует.");
         }
         return true;
     }
